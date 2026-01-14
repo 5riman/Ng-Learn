c162476852ff7a81e9238aa46f85d14bea1a9ba4
5riman <cheetisriman@gmail.com>
2026-01-14 10:42:59 +0530

    learning through handson

---
+++ src/app/loginpage/loginpage.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-loginpage',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './loginpage.html',
+  styleUrls: ['./loginpage.css']
+})
+export class LoginpageComponent { }
